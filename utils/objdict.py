dcb589f2e3e702318903647a2ffc9fe3f07f7e7b
lucien1011 <lucien1011@gmail.com>
2021-05-01 16:35:34 -0400

    Added utils

---
+++ utils/objdict.py
@@ -0,0 +1,23 @@
+class ObjDict(dict):
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    @classmethod
+    def read_from_file_python3(cls,input_path):
+        import importlib
+        spec = importlib.util.spec_from_file_location("cfg",input_path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        return mod.config
