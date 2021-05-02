f7a77ac793af43224130f9c51b9d0a97b98e2b3c
lucien1011 <lucien1011@gmail.com>
2021-05-02 17:44:45 -0400

    Save history.history

--- pipeline/pipeline_classifier.py
+++ pipeline/pipeline_classifier.py
@@ -39,26 +39,7 @@ class ClassifierPipeline(Pipeline):
 
     def save(self):
         self.cfg.model.save(self.cfg.saved_model_path)
-        pickle.dump(self.cfg.history,open(self.cfg.saved_history_path,"wb"))
+        pickle.dump(self.cfg.history.history,open(self.cfg.saved_history_path,"wb"))
 
     def custom_train(self):
         pass
-        #for epoch in range(0):
-        #    
-        #    print("Epoch ",epoch)
-        #   
-        #    idx_batch = np.random.randint(0, x_train.shape[0], 512)
-        #
-        #    with tf.GradientTape() as tape:
-        #        y_train_pred = model(x_train[idx_batch])
-        #        train_loss = loss(y_train[idx_batch],y_train_pred)
-        #    grads = tape.gradient(train_loss,model.trainable_variables)
-        #    optimizer.apply_gradients(zip(grads,model.trainable_variables))
-        #    y_val_pred = model(x_val)
-        #    val_loss = loss(y_val,y_val_pred)
-        #    
-        #    print("Loss: ",train_loss,val_loss)
-        #
-        #    m_train.update_state(y_train[idx_batch],y_train_pred)
-        #    m_val.update_state(y_val,y_val_pred)
-        #    print("AUC: ",m_train.result().numpy(),m_val.result().numpy())
