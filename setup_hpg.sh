5ad6acb1263c9272c898eb8d0884483dc029d66a
lucien1011 <lucien1011@gmail.com>
2021-05-06 07:20:22 -0400

    Update setup_hpg.sh to python with torch and huggingface

--- setup_hpg.sh
+++ setup_hpg.sh
@@ -3,6 +3,8 @@ export BASE_PATH=${PWD}
 
 if [[ "$HOSTNAME" == login*ufhpc ]]; then
     echo "Loading modules"
-    module load tensorflow/2.4.1
+    #module load tensorflow/2.4.1
+    module load python/3.8
+    module load cuda/11.1.0
     module load git
 fi
