0fa3ddf929494838539252f17390202ada1cb4d3
lucien1011 <lucien1011@gmail.com>
2021-05-06 17:24:08 -0400

    update token classifier

--- config/optimise_tokenclassification_210506_01.py
+++ config/optimise_tokenclassification_210506_01.py
@@ -7,8 +7,9 @@ from pipeline.pipeline_tokenclassifier import TokenClassifierPipeline
 from utils.objdict import ObjDict
 
 # __________________________________________________________________ ||
-name = "optimise_tokenclassification_210505"
+name = "optimise_tokenclassification_210506_01_hasDataset_lite"
 model_checkpoint = "distilbert-base-uncased"
+saved_model_path = os.path.join('saved_model/',name,)
 label_list = [0,1]
 batch_size = 16
 
@@ -47,16 +48,33 @@ config.model_checkpoint = model_checkpoint
 config.tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 config.model = AutoModelForTokenClassification.from_pretrained(model_checkpoint, num_labels=len(label_list))
 config.train_args = TrainingArguments(
-    name,
+    output_dir=saved_model_path,
     evaluation_strategy = "epoch",
     learning_rate=2e-5,
     per_device_train_batch_size=batch_size,
     per_device_eval_batch_size=batch_size,
+    do_train=True,
+    do_eval=True,
     num_train_epochs=10,
     weight_decay=0.01,
 )
 config.compute_metrics = compute_metrics
 config.data_collator = DataCollatorForTokenClassification(config.tokenizer)
 
-config.saved_model_path = os.path.join('saved_model/',name)
+config.saved_model_path = saved_model_path
+
+# __________________________________________________________________ ||
+config.slurm_cfg_name = 'submit.cfg'
+config.slurm_job_dir = os.path.join('job/',config.name+'/')
+config.slurm_commands = """echo \"{job_name}\"
+cd {base_path}
+source setup_hpg.sh
+python3 {pyscript} {cfg_path}
+""".format(
+            job_name=config.name,
+            pyscript="run_pipeline.py",
+            cfg_path="config/"+config.name+".py",
+            base_path=os.environ['BASE_PATH'],
+            output_path=config.slurm_job_dir,
+            )
 
--- pipeline/pipeline_tokenclassifier.py
+++ pipeline/pipeline_tokenclassifier.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import torch
 import numpy as np
 
 from datasets import load_from_disk
@@ -16,7 +17,7 @@ class TokenClassifierPipeline(Pipeline):
         self.dataset_dict = self.processed_dataset['train'].train_test_split(test_size=self.cfg.train_test_split)
 
     def train(self):
-        trainer = Trainer(
+        self.trainer = Trainer(
             self.cfg.model,
             self.cfg.train_args,
             train_dataset=self.dataset_dict['train'],
@@ -25,10 +26,11 @@ class TokenClassifierPipeline(Pipeline):
             tokenizer=self.cfg.tokenizer,
             compute_metrics=self.cfg.compute_metrics,
         )
-        trainer.train()
+        self.trainer.train()
 
     def save(self):
         self.cfg.model.save_pretrained(self.cfg.saved_model_path)
+        #torch.save(self.cfg.model,self.cfg.saved_model_path)
 
     def custom_train(self):
         pass
