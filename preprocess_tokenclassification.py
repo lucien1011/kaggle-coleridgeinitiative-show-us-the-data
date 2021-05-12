1d0946cbfaf149d18c4e1c71ac90c6736f3d1b7d
lucien1011 <lucien1011@gmail.com>
2021-05-12 18:00:57 -0400

    Update preprocess_tokenclassification.py

--- preprocess_tokenclassification.py
+++ preprocess_tokenclassification.py
@@ -3,17 +3,13 @@ import json
 import sys
 import pandas as pd
 
-from pipeline.pipeline_tokenclassifier import TokenClassifierPipeline
 from utils.objdict import ObjDict
 
 # __________________________________________________________________ ||
 cfg = ObjDict.read_all_from_file_python3(sys.argv[1])
 
 # __________________________________________________________________ ||
-pp = TokenClassifierPipeline()
+pp = cfg.pipeline
 
-_ = pp.create_preprocess_sequence_train_data(cfg.preprocess_cfg)
-_ = pp.create_preprocess_sequence_test_data(cfg.preprocess_cfg)
-
-#_ = pp.create_preprocess_sentence_train_data(cfg.preprocess_cfg)
-#_ = pp.create_preprocess_sentence_test_data(cfg.preprocess_cfg)
+_ = pp.create_preprocess_train_data(cfg.preprocess_cfg)
+_ = pp.create_preprocess_test_data(cfg.preprocess_cfg)
