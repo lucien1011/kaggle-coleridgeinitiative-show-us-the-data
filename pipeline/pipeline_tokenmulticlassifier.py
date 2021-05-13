9cce23098847c94c51372a0b59e835610c22f5e3
lucien1011 <lucien1011@gmail.com>
2021-05-13 17:57:45 -0400

    Update pipeline_tokenmulticlassifier.py

--- pipeline/pipeline_tokenmulticlassifier.py
+++ pipeline/pipeline_tokenmulticlassifier.py
@@ -32,42 +32,44 @@ def make_label(input_ids,dataset_ids,length):
     start_index = 1
     dataset_length = len(dataset_ids)
     seq_length = len(input_ids)
-    found = False
+    found_indices = []
 
     while start_index + dataset_length < seq_length:
         if not all([input_ids[start_index+i] == dataset_ids[i] for i in range(dataset_length)]):
-            start_index += 1
+            found_indices.append(start_index)
+            start_index += dataset_length
         else:
             found = True
             break
 
     output = [0 for _ in range(length)]
-    if found:
-        for i in range(dataset_length):
-            if i == 0:
-                output[start_index+i] = 1
-            elif i == dataset_length - 1:
-                output[start_index+i] = 3
-            else:
-                output[start_index+i] = 2
+    if found_indices:
+        for start_index in found_indices:
+            for i in range(dataset_length):
+                if i == 0:
+                    output[start_index+i] = 1
+                elif i == dataset_length - 1:
+                    output[start_index+i] = 3
+                else:
+                    output[start_index+i] = 2
 
     return output
 
-def compute_metrics(preds,labels):
-    probs = softmax(preds.logits)[:,:,1].flatten()
-    labels_flatten = labels.flatten()
+def compute_metrics(preds,labels,num_classes):
+    probs = softmax(preds.logits).flatten(0,1)
+    labels_flatten = labels.flatten(0,1)
     return {
-        "accuracy": accuracy(probs,labels_flatten),
+        "accuracy": accuracy(probs,labels_flatten,num_classes=num_classes),
         #"auroc": auroc(probs,labels_flatten),
-        "f1": f1(probs,labels_flatten),
-        "precision": precision(probs,labels_flatten),
-        "recall": recall(probs,labels_flatten),
-        "specificity": specificity(probs,labels_flatten),
+        "f1": f1(probs,labels_flatten,num_classes=num_classes),
+        "precision": precision(probs,labels_flatten,num_classes=num_classes),
+        "recall": recall(probs,labels_flatten,num_classes=num_classes),
+        #"specificity": specificity(probs,labels,num_classes=num_classes),
     }
 
 class TokenMultiClassifierPipeline(Pipeline):
 
-    def preprocess_train_data(self,args):
+    def preprocess(self,args):
         if args.load_preprocess:
             return self.load_preprocess_train_data(args)
         else:
@@ -76,7 +78,7 @@ class TokenMultiClassifierPipeline(Pipeline):
     def load_preprocess_train_data(self,args):
         input_ids = torch.load(os.path.join(args.preprocess_train_dir,"input_ids.pt"))
         attention_mask = torch.load(os.path.join(args.preprocess_train_dir,"attention_mask.pt"))
-        labels = torch.load(os.path.join(args.preprocess_train_dir,"labels.pt"))
+        labels = torch.load(os.path.join(args.preprocess_train_dir,"multilabels.pt"))
 
         dataset = TensorDataset(input_ids,attention_mask,labels)
         train_size = int(args.train_size * len(dataset))
@@ -98,10 +100,15 @@ class TokenMultiClassifierPipeline(Pipeline):
         tokenizer = args.tokenizer
         df = pd.read_csv(args.train_csv_path)
 
-        print("Tokenize text")
-        tokenized_inputs = tokenizer(df['text'].tolist(),pad_to_max_length=True,max_length=512,truncation=True,return_overflowing_tokens=True,return_tensors='pt',)
+        self.print_header()
+        print("Tokenize text ")
+        print("Dataframe shape: ",df.shape)
+        self.print_header()
+        tokenized_inputs = tokenizer(df['text'].tolist(),padding='max_length',max_length=512,truncation=True,return_overflowing_tokens=True,return_tensors='pt',)
 
+        self.print_header()
         print("Make labels")
+        self.print_header()
         labels = []
         ntext = len(tokenized_inputs['overflow_to_sample_mapping'])
         for i in tqdm(range(ntext)):
@@ -109,7 +116,9 @@ class TokenMultiClassifierPipeline(Pipeline):
             labels.append(make_label(tokenized_inputs.input_ids[i],tokenized_dataset['input_ids'][1:-1],len(tokenized_inputs.attention_mask[i]),))
         tokenized_inputs['labels'] = torch.tensor(labels)
 
+        self.print_header()
         print("Saving")
+        self.print_header()
         if args.preprocess_train_dir:
             mkdir_p(args.preprocess_train_dir)
             torch.save(tokenized_inputs['input_ids'],os.path.join(args.preprocess_train_dir,"input_ids.pt"))
@@ -146,7 +155,7 @@ class TokenMultiClassifierPipeline(Pipeline):
         return tokenized_inputs
     
     def load_preprocess_test_data(self,args):
-        ids = torch.load(os.path.join(args.preprocess_test_dir,"id.pt"))
+        #ids = torch.load(os.path.join(args.preprocess_test_dir,"id.pt"))
         input_ids = torch.load(os.path.join(args.preprocess_test_dir,"input_ids.pt"))
         attention_mask = torch.load(os.path.join(args.preprocess_test_dir,"attention_mask.pt"))
 
@@ -155,7 +164,7 @@ class TokenMultiClassifierPipeline(Pipeline):
                 test_dataset = dataset,
                 input_ids = input_ids,
                 attention_mask = attention_mask,
-                ids = ids,
+                #ids = ids,
                 )
         return inputs
 
@@ -229,8 +238,8 @@ class TokenMultiClassifierPipeline(Pipeline):
                             preds_val = model(**batch_val)
                             val_loss = preds_val[0]
                             preds_train = model(**batch_train)
-                            metrics_train = compute_metrics(preds_train,batch_train['labels'])
-                            metrics_val = compute_metrics(preds_val,batch_val['labels'])
+                            metrics_train = compute_metrics(preds_train,batch_train['labels'],model.num_labels)
+                            metrics_val = compute_metrics(preds_val,batch_val['labels'],model.num_labels)
 
                         tqdm.write("*"*100)
                         tqdm.write("global step {global_step}".format(global_step=global_step))
