0bdcf175b69a8f34ecee4d63ec906eea35c87757
lucien1011 <lucien1011@gmail.com>
2021-05-15 13:28:31 -0400

    Update evaluate and extract

--- evaluate_tokenclassification.py
+++ evaluate_tokenclassification.py
@@ -2,10 +2,8 @@ import os
 import sys
 import torch
 
-from transformers import AutoConfig,AutoTokenizer,AutoModelForTokenClassification
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, TensorDataset
 
-from pipeline.pipeline_tokenclassifier import compute_metrics
 from utils.objdict import ObjDict
 from utils.mkdir_p import mkdir_p
 
@@ -13,8 +11,8 @@ from utils.mkdir_p import mkdir_p
 cfg = ObjDict.read_all_from_file_python3(sys.argv[1])
 
 # __________________________________________________________________ ||
-inputs = cfg.pipeline.load_preprocess_sequence_train_data(cfg.preprocess_cfg)
-model = AutoModelForTokenClassification.from_pretrained(cfg.evaluate_cfg.pretrain_model,config=AutoConfig.from_pretrained(cfg.base_pretrained))
+inputs = cfg.pipeline.load_preprocess_train_data(cfg.preprocess_cfg)
+model = cfg.model.from_pretrained(cfg.extract_cfg.pretrain_model)
 
 # __________________________________________________________________ ||
 model = model.to(cfg.evaluate_cfg.device)
@@ -22,7 +20,6 @@ model = model.to(cfg.evaluate_cfg.device)
 test_sampler = RandomSampler(inputs.test_dataset)
 test_dataloader = DataLoader(inputs.test_dataset, sampler=test_sampler, batch_size=cfg.evaluate_cfg.batch_size)
 
-softmax = torch.nn.Softmax(dim=-1)
 with torch.no_grad():
     for step, batch in enumerate(test_dataloader):
         if step % cfg.evaluate_cfg.print_per_step == 0:
@@ -30,7 +27,7 @@ with torch.no_grad():
             batch_test = {"input_ids": batch[0],"attention_mask": batch[1],"labels": batch[2]}
             with torch.no_grad():
                 preds = model(**batch_test)
-                d = compute_metrics(preds,batch_test['labels'])
+                d = cfg.pipeline.compute_metrics(preds,batch_test['labels'])
             print("*"*50)
             print("step ",step)
             print("*"*50)
--- extract_tokenclassification.py
+++ extract_tokenclassification.py
@@ -3,10 +3,8 @@ import sys
 import torch
 from tqdm import tqdm
 
-from transformers import AutoConfig,AutoTokenizer,AutoModelForTokenClassification
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, TensorDataset
 
-from pipeline.pipeline_tokenclassifier import compute_metrics
 from utils.objdict import ObjDict
 from utils.mkdir_p import mkdir_p
 
@@ -18,16 +16,15 @@ batch_size = 1
 
 # __________________________________________________________________ ||
 if cfg.extract_cfg.test:
-    inputs = cfg.pipeline.load_preprocess_sequence_test_data(cfg.preprocess_cfg)
+    inputs = cfg.pipeline.load_preprocess_test_data(cfg.preprocess_cfg)
 else:
-    inputs = cfg.pipeline.load_preprocess_sequence_train_data(cfg.preprocess_cfg)
-model = AutoModelForTokenClassification.from_pretrained(cfg.extract_cfg.pretrain_model,config=AutoConfig.from_pretrained(cfg.base_pretrained))
+    inputs = cfg.pipeline.load_preprocess_train_data(cfg.preprocess_cfg)
+model = cfg.model.from_pretrained(cfg.extract_cfg.pretrain_model)
 
 # __________________________________________________________________ ||
 model = model.to(cfg.extract_cfg.device)
 softmax = torch.nn.Softmax(dim=-1)
 t = open(cfg.extract_cfg.extract_text_path,"w")
-#for step,(input_id,mask) in enumerate(tqdm(zip(inputs.input_ids,inputs.attention_mask))):
 for step in tqdm(range(len(inputs.input_ids))):
     input_id,mask = inputs.input_ids[step],inputs.attention_mask[step]
     if step % cfg.extract_cfg.write_per_step != 0: continue
@@ -37,12 +34,13 @@ for step in tqdm(range(len(inputs.input_ids))):
         preds = model(input_ids=torch.unsqueeze(input_id,0),attention_mask=torch.unsqueeze(mask,0))
         probs = softmax(preds.logits)
         labels = torch.argmax(probs,axis=2)
-    highlighted_tokens = [str(t) for i,t in enumerate(cfg.tokenizer.convert_ids_to_tokens(input_id)) if labels[0,i] == 1]
+    highlighted_tokens = [str(t) for i,t in enumerate(cfg.tokenizer.convert_ids_to_tokens(input_id)) if labels[0,i] != 0]
+    tqdm.write(str(highlighted_tokens))
     t.write("\n".join([
         "*"*50,
         "step "+str(step),
-        str(inputs.ids[step]) if cfg.extract_cfg.test else "",
-        "Number of predicted label: "+str(torch.sum(labels)),
+        #str(inputs.ids[step]) if cfg.extract_cfg.test else "",
+        "Number of predicted label: "+str(int(torch.sum(labels))),
         "*"*50,
         " ".join(cfg.tokenizer.convert_ids_to_tokens(input_id)),
         "*"*50,
