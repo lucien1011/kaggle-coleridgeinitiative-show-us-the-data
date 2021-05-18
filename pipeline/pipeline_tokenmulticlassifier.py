000ea410e5b0310773f7adc274ca0aafe147bc48
lucien1011 <lucien1011@gmail.com>
2021-05-18 11:08:55 -0400

    Added minimum cosine similarity

--- pipeline/pipeline_tokenmulticlassifier.py
+++ pipeline/pipeline_tokenmulticlassifier.py
@@ -19,6 +19,9 @@ from utils.objdict import ObjDict
 logger = logging.getLogger(__name__)
 
 softmax = torch.nn.Softmax(dim=-1)
+        
+from torch.nn import CosineSimilarity
+cos = CosineSimilarity(dim=2,eps=1e-6)
 
 def make_label(input_ids,dataset_ids,length):
     start_index = 1
@@ -177,3 +180,57 @@ class TokenMultiClassifierPipeline(Pipeline):
                 #ids = ids,
                 )
         return inputs
+
+    def evaluate(self,inputs,model,args):
+        from torch.utils.data import DataLoader, RandomSampler 
+        model = model.from_pretrained(args.pretrain_model).to(args.device)
+        dataset_inputs = torch.load(args.dataset_tokens_path)
+        test_sampler = RandomSampler(inputs.test_dataset)
+        test_dataloader = DataLoader(inputs.test_dataset, sampler=test_sampler, batch_size=args.batch_size)
+        with torch.no_grad():
+            dataset_emb = model(
+                    input_ids=dataset_inputs['input_ids'].to(args.device),
+                    attention_mask=dataset_inputs['attention_mask'].to(args.device),
+                    output_hidden_states=True,
+                    ).hidden_states[-1]
+            seq_length = int(dataset_emb.size(1))
+
+            min_context_sims = []
+            
+            for step, batch in enumerate(test_dataloader):
+                if step % args.print_per_step == 0:
+                    batch = tuple(t.to(args.device) for t in batch)
+                    batch_test = {"input_ids": batch[0],"attention_mask": batch[1], "output_hidden_states": True,}
+                    batch_model_out = model(**batch_test)
+                    
+                    _,pred_label_idx = torch.split((torch.argmax(batch_model_out.logits,axis=2)!=0).nonzero(),1,dim=1,)
+                    pred_label_idx = torch.squeeze(pred_label_idx)
+
+                    if pred_label_idx.numel():
+                        pred_label = torch.squeeze(batch_test['input_ids'])[pred_label_idx]
+                        min_context_sims.append(self.calculate_min_context_similarity(model,dataset_emb,pred_label))
+            self.print_header()
+            print("minimum context similarity / number of prediction: {cos} / {npred:d}".format(
+                cos=str(torch.mean(torch.stack(min_context_sims))),
+                npred=len(min_context_sims),
+                )
+                )
+            self.print_header()
+
+
+    @classmethod
+    def calculate_min_context_similarity(cls,model,dataset_emb,pred_label):
+        """
+        dataset_emb: [ number of dataset names , sequence_length , embedding_dimension ]
+        pred_label: [ number of predicted tokens ]
+        model: embedding model
+        """
+        pred_length = len(pred_label)
+        pred_length = pred_label.numel()
+        seq_length = int(dataset_emb.size(1))
+        pred_label = torch.nn.functional.pad(pred_label,pad=(0,seq_length-pred_length))
+        pred_emb = model(input_ids=torch.unsqueeze(pred_label,axis=0),output_hidden_states=True).hidden_states[-1]
+        sim_matrix = cos(dataset_emb,pred_emb)
+        min_context_sim = torch.min(torch.sqrt(torch.sum(sim_matrix**2,axis=1)))
+        return min_context_sim
+
