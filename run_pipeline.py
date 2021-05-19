54a86832af2500306b947bebc9971dc60c5add4f
lucien1011 <lucien1011@gmail.com>
2021-05-19 11:07:09 -0400

    Update run_pipeline.py

--- run_pipeline.py
+++ run_pipeline.py
@@ -5,5 +5,7 @@ from utils.objdict import ObjDict
 cfg = ObjDict.read_all_from_file_python3(sys.argv[1])
 
 pipeline = cfg.pipeline
+if not cfg.preprocess_cfg.load_preprocess:
+    _ = pipeline.create_preprocess_train_data(cfg.preprocess_cfg)
 inputs = pipeline.load_preprocess_train_data(cfg.preprocess_cfg)
 pipeline.train(inputs,cfg.model,cfg.train_cfg)
