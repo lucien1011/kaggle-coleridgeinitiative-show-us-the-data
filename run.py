05b2e1599aba7c4307a2d829d6cdabbc7409fb5d
lucien1011 <lucien1011@gmail.com>
2021-05-24 10:28:27 -0400

    parsearg in run.py

--- run.py
+++ run.py
@@ -1,23 +1,44 @@
+import argparse
 import sys
 
 from utils.objdict import ObjDict
 
-cfg = ObjDict.read_all_from_file_python3(sys.argv[1])
+job_keywords = [
+        "create_data",
+        "load_data",
+        "train_data",
+        "predict",
+        "evaluate",
+        ]
 
-pipeline = cfg.pipeline
-jobs = sys.argv[2].split(",")
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cfg')
+    parser.add_argument('jobs')
+    return parser.parse_args()
 
-if "create_data" in jobs:
-    inputs = pipeline.create_preprocess_train_data(cfg.preprocess_cfg)
+if __name__ == "__main__":
 
-if "load_data" in jobs:
-    inputs = pipeline.load_preprocess_train_data(cfg.preprocess_cfg)
+    args = parse_arguments()
 
-if "train" in jobs:
-    pipeline.train(inputs,cfg.model,cfg.train_cfg)
+    cfg = ObjDict.read_all_from_file_python3(args.cfg)
+    
+    pipeline = cfg.pipeline
+    jobs = args.jobs.split(",")
 
-if "predict" in jobs:
-    pipeline.predict(inputs,cfg.model,cfg.predict_cfg)
-
-if "evaluate" in jobs:
-    pipeline.evaluate(inputs,cfg.model,cfg.evaluate_cfg)
+    assert all([j in job_keywords for j in jobs])
+    
+    if "create_data" in jobs:
+        inputs = pipeline.create_preprocess_train_data(cfg.preprocess_cfg)
+    
+    if "load_data" in jobs:
+        inputs = pipeline.load_preprocess_train_data(cfg.preprocess_cfg)
+    
+    if "train" in jobs:
+        pipeline.train(inputs,cfg.model,cfg.train_cfg)
+    
+    if "predict" in jobs:
+        pipeline.predict(inputs,cfg.model,cfg.predict_cfg)
+    
+    if "evaluate" in jobs:
+        pipeline.evaluate(inputs,cfg.model,cfg.evaluate_cfg)
