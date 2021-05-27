49b719e9053f19e25e7f7dcbe0034d8a83055537
lucien1011 <lucien1011@gmail.com>
2021-05-27 10:59:55 -0400

    Added add_id option

--- postprocessing/make_text_from_saved_ids.py
+++ postprocessing/make_text_from_saved_ids.py
@@ -2,27 +2,34 @@ import os
 import glob
 import argparse
 
+import pandas as pd
 import torch
 
 from utils.objdict import ObjDict
 from utils.mkdir_p import mkdir_p
 
 next_line = '\n'
+saved_label_key = "pred_ids"
+of_mapping_key = "overflow_mapping"
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('input_path',type=str)
     parser.add_argument('--write_dataset_only',action='store_true')
+    parser.add_argument('--add_id',action='store_true')
     return parser.parse_args()
 
-def convert_ids_to_string(cfg,device='cuda',out_str='pred_str.txt',write_dataset_only=False):
+def convert_ids_to_string(cfg,device='cuda',out_str='pred_str.txt',write_dataset_only=False,add_id=False):
     pp = cfg.pipeline
     out_dict = {}
     checkpts = pp.get_model_checkpts(cfg.extract_cfg.model_dir,cfg.extract_cfg.model_key)
+    if add_id:
+        textids = pd.read_csv(cfg.preprocess_cfg.test_csv_path).id
     
     outfile = open(os.path.join(cfg.extract_cfg.output_dir,out_str),"w")
     checkpts.sort()
     for c in checkpts:
+        print("Processing checkpoint "+c)
         outfile.write("*"*100+next_line)
         outfile.write(c+next_line)
         outfile.write("*"*100+next_line)
@@ -31,17 +38,25 @@ def convert_ids_to_string(cfg,device='cuda',out_str='pred_str.txt',write_dataset
             print(cdir+" not exist")
             continue
         fs = [f for f in os.listdir(cdir) if ".pt" in f]
+        fs.sort()
         if not fs:
             print("empty folder: {}, skipping".format(c))
             continue
-        pred_ids = torch.cat([torch.load(os.path.join(cdir,f)) for f in fs])
+        pred_ids = torch.cat([torch.load(os.path.join(cdir,f)) for f in fs if saved_label_key in f])
+        if add_id:
+            ids = torch.cat([torch.load(os.path.join(cdir,f)) for f in fs if of_mapping_key in f])
         pred_ids[pred_ids==-1] = 0
         if write_dataset_only:
-            pred_ids = pred_ids[torch.sum(pred_ids,axis=1)!=0]
+            pred_idx = torch.sum(pred_ids,axis=1)!=0
+            pred_ids = pred_ids[pred_idx]
+            ids = ids[pred_idx]
         pp.print_message(c)
 
         strs = cfg.tokenizer.batch_decode(pred_ids,skip_special_tokens=True)
-        outfile.write(next_line.join([str(i)+", "+s for i,s in enumerate(strs) if s]))
+        if add_id:
+            outfile.write(next_line.join([str(textids[int(ids[i])])+": "+str(i)+", "+s for i,s in enumerate(strs) if s]))
+        else:
+            outfile.write(next_line.join([str(i)+", "+s for i,s in enumerate(strs) if s]))
         outfile.write(next_line)
     outfile.close()
 
@@ -61,4 +76,4 @@ if __name__ == "__main__":
     
     data = {}
     for c in cfgs:
-        convert_ids_to_string(c,write_dataset_only=args.write_dataset_only)        
+        convert_ids_to_string(c,write_dataset_only=args.write_dataset_only,add_id=args.add_id)        
